504ab5c9e17cd8f58b5d41629940227101d4fa68
Williams <hugh.williams@C02PRY31G8WP.servicemagic.com>
2019-10-22 09:29:34 -0600

    Bug Fixes

--- tictactoe.py
+++ tictactoe.py
@@ -31,21 +31,21 @@ def place_marker(board, marker, position):
 
 
 def win_check(board, mark):
-    if board[7] == mark and board[8] == mark and board[9] == mark:
+    if board[7] == board[8] == board[9] == mark:
         return True
-    elif board[7] == mark and board[4] == mark and board[1] == mark:
+    elif board[7] == board[4] == board[1] == mark:
         return True
-    elif board[7] == mark and board[5] == mark and board[3] == mark:
+    elif board[7] == board[5] == board[3] == mark:
         return True
-    elif board[1] == mark and board[5] == mark and board[9] == mark:
+    elif board[1] == board[5] == board[9] == mark:
         return True
-    elif board[1] == mark and board[2] == mark and board[3] == mark:
+    elif board[1] == board[2] == board[3] == mark:
         return True
-    elif board[4] == mark and board[5] == mark and board[6] == mark:
+    elif board[4] == board[5] == board[6] == mark:
         return True
-    elif board[8] == mark and board[5] == mark and board[2] == mark:
+    elif board[8] == board[5] == board[2] == mark:
         return True
-    elif board[9] == mark and board[6] == mark and board[3] == mark:
+    elif board[9] == board[6] == board[3] == mark:
         return True
     else:
         return False
@@ -95,18 +95,16 @@ def replay():
 
 print('Welcome to Tic Tac Toe!')
 board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-playerChoice = player_input()
-playerOneLetter = playerChoice[0]
-playerTwoLetter = playerChoice[1]
-firstMove = choose_first()
+playerOneLetter , playerTwoLetter = player_input()
+nextMove = choose_first()
 playerOneMove = True
-print("\n" + firstMove + ' will go first!\n\n')
-if firstMove == 'Player 2':
+if nextMove == 'Player 2':
     playerOneMove = False
 
 while True:
     gameOver = False
     display_board(board)
+    print("\n" + nextMove + ' is next!\n\n')
     if playerOneMove:
         position = player_choice(board)
         place_marker(board,playerOneLetter,position)
@@ -119,6 +117,7 @@ while True:
             print("Board is full. Nobody wins. You both are losers!")
             gameOver = True
         playerOneMove = False
+        nextMove = 'Player 2'
     else:
         position = player_choice(board)
         place_marker(board,playerTwoLetter,position)
@@ -131,14 +130,14 @@ while True:
             print("Board is full. Nobody wins. You both are losers!")
             gameOver = True
         playerOneMove = True
+        nextMove = 'Player 1'
         
     if gameOver:
         if replay():
             gameOver = False
             board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-            firstMove = choose_first()
-            print("\n" + firstMove + ' will go first!\n\n')
-            if firstMove == 'Player 2':
+            nextMove = choose_first()
+            if nextMove == 'Player 2':
                 playerOneMove = False
             else:
                 playerOneMove = True
